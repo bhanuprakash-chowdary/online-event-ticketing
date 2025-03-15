4af517be26c68419a50ad1fda96afe24df395ede
R Bhanuprakash Chowdary <rbchowdary1507@gmail.com>
2025-03-16 01:18:34 +0530

    OETP with ReentrantLock - finer concurrency control

--- src/main/java/com/oetp/Main.java
+++ src/main/java/com/oetp/Main.java
@@ -3,23 +3,51 @@ package com.oetp;
 import com.oetp.domain.Event;
 import com.oetp.service.TicketService;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Event concert = new Event(1, "ColdPlay Concert", 10);
 
         TicketService service = new TicketService(concert);
 
-        Thread thread1 = new Thread(() -> service.bookTickets("User 1", 3));
+        ExecutorService pool= Executors.newFixedThreadPool(5);
 
-        Thread thread2 = new Thread() {
-            @Override
-            public void run() {
-                service.bookTickets("User 2", 3);
-            }
-        };
+        pool.submit(() -> service.bookTicket("User1", 3));
+        pool.submit(() -> service.bookTicket("User2", 3));
+        pool.submit(() -> service.bookTicket("User3", 3));
+        pool.submit(() -> service.bookTicket("User4", 3));
+        pool.submit(() -> service.bookTicket("User5", 3));
+        pool.submit(() -> service.bookTicket("User6", 3));
+        pool.submit(() -> service.bookTicket("User7", 3));
+        pool.submit(() -> service.bookTicket("User8", 3));
+        pool.submit(() -> service.bookTicket("User9", 3));
 
-        thread1.start();
-        thread2.start();
+
+        pool.shutdown();
+        pool.awaitTermination(5, TimeUnit.SECONDS); // Wait up to 5s
+        System.out.println("All bookings done. Final tickets: " + concert.getAvailableTickets());
+
+//        Thread thread1 = new Thread(() -> service.bookTicket("User 1", 3));
+//
+//        Thread thread2 = new Thread() {
+//            @Override
+//            public void run() {
+//                service.bookTicket("User 2", 3);
+//            }
+//        };
+//
+//        Thread thread3 = new Thread(() -> service.bookTicket("User 3", 3));
+//        Thread thread4 = new Thread(() -> service.bookTicket("User 4", 3));
+//        Thread thread5 = new Thread(() -> service.bookTicket("User 5", 3));
+//
+//        thread1.start();
+//        thread2.start();
+//        thread3.start();
+//        thread4.start();
+//        thread5.start();
     }
 }
--- src/main/java/com/oetp/service/TicketService.java
+++ src/main/java/com/oetp/service/TicketService.java
@@ -2,30 +2,42 @@ package com.oetp.service;
 
 import com.oetp.domain.Event;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TicketService {
 
     private final Event event;
+    private final ReentrantLock lock=new ReentrantLock();
 
     public TicketService(Event event){
         this.event=event;
     }
 
-    public void bookTickets(String user,int quantity){
+    public void bookTicket(String user,int quantity) { //synchronize locks whole function
 
-       if(event.getAvailableTickets()>=quantity){
+        System.out.println(user + " checking " + quantity + " for " + event.getName() + "( Available at the moment " + event.getAvailableTickets() + ")");
 
-           System.out.println(user +" booking "+ quantity +" for "+ event.getName()+ "( Available at the moment "+ event.getAvailableTickets()+")");
+        if (event.getAvailableTickets() >= quantity) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            lock.lock();//lock only reduce Tickets (not whole method including sleep )
+            try {
+                if (event.getAvailableTickets() >= quantity) {
 
-           try {
-               Thread.sleep(100);
-           }catch (InterruptedException e) {
-               e.printStackTrace();
-           }
-           event.reduceTickets(quantity);
-           System.out.println(user + " booked " + quantity + ". Remaining: " + event.getAvailableTickets());
+                    event.reduceTickets(quantity);
+                    System.out.println(user + " booked " + quantity + ". Remaining: " + event.getAvailableTickets());
 
-       }else {
-           System.out.println(user + " failed - not enough tickets.");
-       }
+                } else {
+                    System.out.println(user + " failed - not enough tickets (race detected).");
+                }
+            } finally {
+                lock.unlock();
+            }
+        } else {
+            System.out.println(user + " failed - not enough tickets.");
+        }
     }
 }
