a6b52cc01bcbddaca4d7cfeb8db34da68206d9da
R Bhanuprakash Chowdary <rbchowdary1507@gmail.com>
2025-03-16 02:16:23 +0530

    AtomicInteger - lock-free concurrency

--- src/main/java/com/oetp/domain/Event.java
+++ src/main/java/com/oetp/domain/Event.java
@@ -1,14 +1,16 @@
 package com.oetp.domain;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Event {
     private final int id;
     private final String name;
-    private int availableTickets;
+    private AtomicInteger availableTickets;
 
     public Event(int id, String name, int availableTickets) {
         this.id = id;
         this.name = name;
-        this.availableTickets = availableTickets;
+        this.availableTickets = new AtomicInteger(availableTickets);
     }
 
     public int getId() {
@@ -20,10 +22,17 @@ public class Event {
     }
 
     public int getAvailableTickets() {
-        return availableTickets;
+        return availableTickets.get();
     }
 
-    public void reduceTickets(int quantity){
-        availableTickets-=quantity;
+    public boolean reduceTickets(int quantity){
+        int current;
+        do{
+            current = availableTickets.get();
+            if(current<quantity){
+                return false;
+            }
+        }while(!availableTickets.compareAndSet(current,current-quantity));
+        return true;
     }
 }
--- src/main/java/com/oetp/service/TicketService.java
+++ src/main/java/com/oetp/service/TicketService.java
@@ -14,30 +14,28 @@ public class TicketService {
     }
 
     public void bookTicket(String user,int quantity) { //synchronize locks whole function
-
         System.out.println(user + " checking " + quantity + " for " + event.getName() + "( Available at the moment " + event.getAvailableTickets() + ")");
-
-        if (event.getAvailableTickets() >= quantity) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            lock.lock();//lock only reduce Tickets (not whole method including sleep )
-            try {
-                if (event.getAvailableTickets() >= quantity) {
-
-                    event.reduceTickets(quantity);
-                    System.out.println(user + " booked " + quantity + ". Remaining: " + event.getAvailableTickets());
-
-                } else {
-                    System.out.println(user + " failed - not enough tickets (race detected).");
-                }
-            } finally {
-                lock.unlock();
-            }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (event.reduceTickets(quantity)) {
+            System.out.println(user + " booked " + quantity + ". Remaining: " + event.getAvailableTickets());
         } else {
             System.out.println(user + " failed - not enough tickets.");
         }
+
+//            lock.lock();//lock only reduce Tickets (not whole method including sleep )
+//            try {
+//                if (event.getAvailableTickets() >= quantity) {
+//                    event.reduceTickets(quantity);
+//                    System.out.println(user + " booked " + quantity + ". Remaining: " + event.getAvailableTickets());
+//                } else {
+//                    System.out.println(user + " failed - not enough tickets (race detected).");
+//                }
+//            } finally {
+//                lock.unlock();
+//            }
     }
 }
