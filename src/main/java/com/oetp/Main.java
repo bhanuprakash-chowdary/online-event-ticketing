94b418b59bd31ea8bbc78a64cdff57ec9b64af5e
R Bhanuprakash Chowdary <rbchowdary1507@gmail.com>
2025-03-16 16:14:47 +0530

    OETP with CyclicBarrier - batch 5 bookings

--- src/main/java/com/oetp/Main.java
+++ src/main/java/com/oetp/Main.java
@@ -12,59 +12,16 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        //command line info
         TicketService service= new TicketService();
-        ExecutorService pool = Executors.newFixedThreadPool(10);
-        Scanner scanner=new Scanner(System.in);
+        ExecutorService pool = Executors.newFixedThreadPool(5);
         service.addEvent(new Event(1,"ColdPlay", 100));
 
-
-//        int userCount = 100;
-//        CountDownLatch latch = new CountDownLatch(userCount);
-//        for (int i = 1; i <= userCount; i++) {
-//            String user = "StressUser" + i;
-//            pool.submit(() -> {
-//                try {
-//                    service.bookTicket(user, 1, 2);
-//                } finally {
-//                    latch.countDown();
-//                }
-//            });
-//        }
-//        latch.await();
-//        System.out.println("Stress test done. Tickets: " + service.getEvent(1).getAvailableTickets());
-
-        System.out.println("OETP CLI Started");
-        while(true) {
-            System.out.println("\nCommands: list, add <id> <name> <tickets>, book <user> <eventId> <tickets>, exit");
-            String input = scanner.nextLine().trim();
-            String [] parts=input.split("\\s");
-
-            if(parts[0].equals("exit")){
-                pool.shutdown();
-                break;
-            }
-            else if (parts[0].equals("list")) {
-                service.listEvents();
-            }else if(parts[0].equals("add") && parts.length==4){
-                int id = Integer.parseInt(parts[1]);
-                String name = parts[2];
-                int tickets = Integer.parseInt(parts[3]);
-                service.addEvent(new Event(id,name,tickets));
-                System.out.println("Added: " + name);
-            }else if(parts[0].equals("book") && parts.length==4){
-
-                String user=parts[1];
-                int eventId=Integer.parseInt(parts[2]);
-                int tickets=Integer.parseInt(parts[3]);
-                service.bookTicket(user,eventId,tickets).thenAccept(System.out::println);//Async
-                System.out.println(user + " booking queued...");
-
-            }else {
-                System.out.println("Invalid command");
-            }
+        for (int i = 1; i <= 10; i++) {
+            String user = "User" + i;
+            pool.submit(()-> System.out.println(service.bookTicket(user, 1, 2)));
         }
-        scanner.close();
 
+        pool.shutdown();
+        pool.awaitTermination(5, TimeUnit.SECONDS);
     }
 }
--- src/main/java/com/oetp/service/TicketService.java
+++ src/main/java/com/oetp/service/TicketService.java
@@ -2,16 +2,16 @@ package com.oetp.service;
 
 import com.oetp.domain.Event;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class TicketService {
 
     private final ConcurrentHashMap<Integer, Event> events;
     private final Semaphore semaphore=new Semaphore(5);
-
+    private final CyclicBarrier barrier = new CyclicBarrier(5,()->System.out.println("Batch of 5 bookings processed!"));
+    private final AtomicInteger batchCounter = new AtomicInteger(0);
     //private final ReentrantLock lock=new ReentrantLock();
 
     public TicketService(){
@@ -36,44 +36,38 @@ public class TicketService {
         return events.get(id);
     }
 
-    public CompletableFuture<String> bookTicket(String user, int eventId, int quantity) { //synchronize locks whole function
+    public String bookTicket(String user, int eventId, int quantity) { //synchronize locks whole function
 
-        return CompletableFuture.supplyAsync(()-> {
+//        return CompletableFuture.supplyAsync(()-> {
             try {
+                barrier.await();
+                batchCounter.incrementAndGet();
                 semaphore.acquire();//grab a permit (wait if none).
                 Event event = events.get(eventId);
                 if (event != null) {
-                    if (event.reduceTickets(quantity)) {
+                    if (event.getAvailableTickets() >= quantity) {
+
                         try {
                             Thread.sleep(100);
-                        }catch (InterruptedException e) {
+                        } catch (InterruptedException e) {
                             Thread.currentThread().interrupt();
                         }
-                        return user + " booked " + quantity + " tickets for " + event.getName() +
-                                ". Remaining: " + event.getAvailableTickets();
+
+                        if (event.reduceTickets(quantity)) {
+                            return user + " booked " + quantity + " tickets for " + event.getName() +
+                                    ". Remaining: " + event.getAvailableTickets();
+                        }
                     }
 
                     return user + " failed - not enough tickets for " + event.getName();
                 }
                 return "Event not found: " + eventId;
-            } catch (InterruptedException e) {
+            } catch (Exception e) {
                 Thread.currentThread().interrupt();
                 return user + " booking interrupted";
-            } finally {
+            }finally {
                 semaphore.release();// Free permit
             }
-        });
-
-//            lock.lock();//lock only reduceTickets method (not whole method including sleep )
-//            try {
-//                if (event.getAvailableTickets() >= quantity) {
-//                    event.reduceTickets(quantity);
-//                    System.out.println(user + " booked " + quantity + ". Remaining: " + event.getAvailableTickets());
-//                } else {
-//                    System.out.println(user + " failed - not enough tickets (race detected).");
-//                }
-//            } finally {
-//                lock.unlock();
-//            }
+//        });
     }
 }
