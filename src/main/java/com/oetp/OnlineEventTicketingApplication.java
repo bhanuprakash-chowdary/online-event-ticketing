bdb60f0cdaaf07b8d762a1df87b69eb4a8983dd6
Ravilla Bhanuprakash Chowdary <ravilla.b@hornerautomation.in>
2025-03-16 19:32:53 +0530

    Initial Spring Boot setup for OETP v2.0

---
+++ src/main/java/com/oetp/OnlineEventTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.oetp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineEventTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineEventTicketingApplication.class, args);
+	}
+
+}
